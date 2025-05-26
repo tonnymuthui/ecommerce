30c77abebe6bfb9313cc02a4237d268e522163ee
tonnymuthui <69537407+tonnymuthui@users.noreply.github.com>
2025-05-26 14:26:56 +0300

    created: authcontext

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,2 +1,37 @@
 import React, {createContext, useEffect, useState} from 'react';
-import { getToken, setToken as saveToken } from './apis/authService'
+import { getToken, setToken as saveToken } from './apis/authService';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [ authToken, setAuthToken] = useState(getToken());
+    const [user, setUser ] = useState(null);
+
+    useEffect(() =>{
+        if (authToken){
+            const [, payload] = authToken.split('.');
+            const decoded = JSON.parse(atob(payload));
+            setUser({ id: decoded.userId, role: decoded.role });
+        }
+    }, [authToken]);
+
+
+    const login = (token) => {
+        saveToken(token);
+        setAuthToken(token);
+    };
+
+    //logout method
+
+    const logout = () => {
+        localStorage.removeItem('authToken');
+        setAuthToken(null);
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ authToken, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
